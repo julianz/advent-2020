a98312758c7740befb15bd1586b1a5ac44c86692
Julian Melville <julian@melville.net.nz>
2020-12-01 19:04:31 +1300

    Day 1 solve

--- Advent/Days/Day01.cs
+++ Advent/Days/Day01.cs
@@ -6,22 +6,42 @@ using System.Text;
 namespace Advent.Days {
     [Day(2020, 1)]
     public class Day01 : DayBase {
-        const char Up = '(';
-        const char Down = ')';
 
         public Day01() {
             NeedsInput = true;
         }
 
         public override string PartOne(string input) {
-            int ups = input.Where(c => c == Up).Count();
-            int downs = input.Where(c => c == Down).Count();
+            var nums = input.Split('\n').Select(n => Int32.Parse(n)).ToList();
+            foreach(var n1 in nums) {
+                var n2 = 2020 - n1;
+                if (nums.Contains(n2)) {
+                    var product = n1 * n2;
+                    Console.WriteLine($"Found: {n1} * {n2} = {product}");
+                    return product.ToString();
+                }
+            }
 
-            return (ups - downs).ToString();
+            return "Not found";
         }
 
         public override string PartTwo(string input) {
-            throw new NotImplementedException();
+            var product = 0;
+            var nums = input.Split('\n').Select(n => Int32.Parse(n)).ToList();
+
+            foreach (var n1 in nums) {
+                foreach (var n2 in nums) {
+                    foreach (var n3 in nums) {
+                        if (n1 + n2 + n3 == 2020) {
+                            product = n1 * n2 * n3;
+                            Console.WriteLine("{0} + {1} + {2} = 2020, product is {3}", n1, n2, n3, product);
+                        }
+                    }
+                }
+            }
+
+            return product.ToString();
         }
     }
 }
+
