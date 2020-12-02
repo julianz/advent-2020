92100c50c4aa8f19f59e053873949dc96ae19a6b
Julian Melville <julian.melville@aderant.com>
2020-12-02 16:33:39 +1300

    Add Day 2 and day template

---
+++ Advent/Days/Day02.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Days {
+    [Day(2020, 2)]
+    public class Day02 : DayBase {
+        public override string PartOne(string input) {
+            throw new NotImplementedException();
+        }
+
+        public override string PartTwo(string input) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Advent/Days/DayTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Days {
+    //[Day(2020, 1)]
+
+    public class DayTemplate : DayBase {
+        public override string PartOne(string input) {
+            throw new NotImplementedException();
+        }
+
+        public override string PartTwo(string input) {
+            throw new NotImplementedException();
+        }
+    }
+}
