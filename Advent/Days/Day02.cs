d54ca4b1448e32e807f8c40dbb0f2f37315cd82a
Julian Melville <julian@melville.net.nz>
2020-12-02 18:58:18 +1300

    Day 2 solve

--- Advent/Days/Day02.cs
+++ Advent/Days/Day02.cs
@@ -2,17 +2,72 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Advent.Util;
 
 namespace Advent.Days {
     [Day(2020, 2)]
     public class Day02 : DayBase {
         public override string PartOne(string input) {
-            throw new NotImplementedException();
+            var valid = 0;
+
+            foreach (var line in input.AsLines()) {
+                var policy = GetPasswordPolicy(line);
+
+                var count = policy.Password.Count(c => c == policy.Letter);
+                if (count >= policy.Min && count <= policy.Max) {
+                    valid++;
+                }
+            }
+            
+            return valid.ToString();
         }
 
         public override string PartTwo(string input) {
-            throw new NotImplementedException();
+            var valid = 0;
+
+            foreach (var line in input.AsLines()) {
+                var policy = GetPasswordPolicy(line);
+
+                // Exclusive OR
+                if (policy.Password[policy.Min - 1] == policy.Letter 
+                    ^ policy.Password[policy.Max - 1] == policy.Letter) {
+
+                    valid++;
+                }
+            }
+
+            return valid.ToString();
+        }
+
+        Policy GetPasswordPolicy(string line) {
+            var bits = line.Split(':', StringSplitOptions.TrimEntries);
+            var policyBits = bits[0].Split(' ');
+            var minmax = policyBits[0].Split('-');
+            var min = Int32.Parse(minmax[0]);
+            var max = Int32.Parse(minmax[1]);
+            char ch = policyBits[1][0];
+            var password = bits[1];
+
+            return new Policy
+            {
+                Min = min,
+                Max = max,
+                Letter = ch,
+                Password = password
+            };
+        }
+
+        class Policy {
+            public int Min { get; set; }
+            public int Max { get; set; }
+            public char Letter { get; set; }
+            public string Password { get; set; }
+
+            public override string ToString() {
+                return $"{Min}-{Max} {Letter}: {Password}";
+            }
         }
     }
 }
