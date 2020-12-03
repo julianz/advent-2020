c9532277e4530405ed7a60586f56917d98c50cc8
Julian Melville <julian@melville.net.nz>
2020-12-03 18:27:56 +1300

    Day 3 solve

--- Advent/Days/Day03.cs
+++ Advent/Days/Day03.cs
@@ -1,20 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Advent.Util;
 
 namespace Advent.Days {
     [Day(2020, 3)]
     public class Day03 : DayBase {
         public override string PartOne(string input) {
-            return "";
+
+            var map = input.AsLines().ToList();
+            var height = map.Count;
+            var col = 0;
+            var row = 0;
+            var trees = 0;
+
+            while (row < height - 1) {
+                col += 3;
+                row += 1;
+                Console.WriteLine($"Moving to {col}, {row}");
+                if (IsTreeAt(map, col, row)) {
+                    trees++;
+                    Console.WriteLine($"Tree!");
+                }
+            }
+
+            return trees.ToString();
         }
 
         public override string PartTwo(string input) {
-            return "";
+//            input = @"..##.......
+//#...#...#..
+//.#....#..#.
+//..#.#...#.#
+//.#...##..#.
+//..#.##.....
+//.#.#.#....#
+//.#........#
+//#.##...#...
+//#...##....#
+//.#..#...#.#";
+
+            var map = input.AsLines().ToList();
+            var height = map.Count;
+            var slopes = new List<(int across, int down)>
+            {
+                (1, 1), (3, 1), (5, 1), (7, 1), (1, 2)
+            };
+
+            var totalTrees = 1; // so the first multiplication works
+
+            foreach (var slope in slopes) {
+                var col = 0;
+                var row = 0;
+                var trees = 0;
+
+                while (row < height - 1) {
+                    col += slope.across;
+                    row += slope.down;
+                    if (IsTreeAt(map, col, row)) {
+                        trees++;
+                    }
+                }
+
+                Console.WriteLine($"Moving {slope.across}x{slope.down} found {trees} trees");
+                totalTrees *= trees;
+            }
+
+            return totalTrees.ToString();
+        }
+
+        private bool IsTreeAt(List<string> map, int col, int row) {
+            var rowData = map[row];
+            var colWrapped = col % rowData.Length;
+            return (rowData[colWrapped] == '#');
         }
     }
 }
