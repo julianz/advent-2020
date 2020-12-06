8aa06dd4366573b281958f8ac60eecaabaaf7eb3
Julian Melville <julian@melville.net.nz>
2020-12-06 18:22:23 +1300

    2015 day 2 solve

---
+++ Advent/Year2015/Day02.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Advent.Util;
+
+namespace Advent.Year2015 {
+    [Day(2015, 2)]
+    public class Day02 : DayBase {
+        public override string PartOne(string input) {
+            var total = 0;
+
+            foreach (var line in input.AsLines()) {
+                var sides = line.Split('x').Select(s => Int32.Parse(s)).ToList();
+                sides.Sort();
+
+                total += (3 * sides[0] * sides[1]) + (2 * sides[1] * sides[2]) + (2 * sides[0] * sides[2]);
+            }
+
+            return total.ToString();
+        }
+
+        public override string PartTwo(string input) {
+            var total = 0;
+
+            foreach (var line in input.AsLines()) {
+                var sides = line.Split('x').Select(s => Int32.Parse(s)).ToList();
+                sides.Sort();
+
+                total += (2 * sides[0]) + (2 * sides[1]) + (sides[0] * sides[1] * sides[2]);
+            }
+
+            return total.ToString();
+        }
+    }
+}
