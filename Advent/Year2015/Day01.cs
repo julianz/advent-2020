2005bd331cbc2a78f37776362e0993a1aa083aa5
Julian Melville <julian@melville.net.nz>
2020-12-06 17:18:19 +1300

    2015 day 1 solve

---
+++ Advent/Year2015/Day01.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Advent.Util;
+
+namespace Advent.Year2015 {
+    [Day(2015, 1)]
+    public class Day01: DayBase {
+        const char Up = '(';
+        const char Down = ')';
+
+        public override string PartOne(string input) {
+            var ups = input.Count(c => c == Up);
+            var downs = input.Count(c => c == Down);
+
+            return (ups - downs).ToString();
+        }
+
+        public override string PartTwo(string input) {
+            var floor = 0;
+            var step = 0;
+
+            foreach (var c in input) {
+                step++;
+                floor += (c == Up) ? 1 : -1;
+
+                if (floor == -1) {
+                    return step.ToString();
+                }
+            }
+
+            return "Not found";
+        }
+    }
+}
