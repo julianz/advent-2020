336864b3736750ec0a17ed35795aa648b63b1758
Julian Melville <julian@melville.net.nz>
2020-12-07 22:55:46 +1300

    2020 day 7 part 2 solve

--- Advent/Year2020/Day07.cs
+++ Advent/Year2020/Day07.cs
@@ -52,8 +52,45 @@ namespace Advent.Year2020 {
             return Containers.Count.ToString();
         }
 
+        Dictionary<string, Dictionary<string, int>> Contains = new Dictionary<string, Dictionary<string, int>>();
+
         public override string PartTwo(string input) {
-            return "";
+            //input = @"light red bags contain 1 bright white bag, 2 muted yellow bags.
+            //        dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+            //        bright white bags contain 1 shiny gold bag.
+            //        muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+            //        shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+            //        dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+            //        vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+            //        faded blue bags contain no other bags.
+            //        dotted black bags contain no other bags.";
+
+            var rules = input.AsLines();
+
+            foreach (var rule in rules) {
+                var split = rule.Split("bags contain", StringSplitOptions.TrimEntries);
+                var containerColour = split[0];
+                var contained = split[1].Split(",", StringSplitOptions.TrimEntries);
+
+                if (contained.Length == 1 && contained[0].StartsWith("no other bags")) {
+                    // not interested
+                    continue;
+                }
+
+                foreach (var bag in contained) {
+                    var bagSplit = bag.Split(" ");
+                    var bagCount = Int32.Parse(bagSplit[0]);
+                    var bagColour = bagSplit[1] + " " + bagSplit[2];
+
+                    if (!Contains.ContainsKey(containerColour)) {
+                        Contains[containerColour] = new Dictionary<string, int>();
+                    }
+
+                    Contains[containerColour][bagColour] = bagCount;
+                }
+            }
+
+            return CountContained("shiny gold").ToString();
         }
 
         void CountContainers(string colour) {
@@ -64,6 +101,21 @@ namespace Advent.Year2020 {
                     CountContainers(container);
                 }
             }
+        }
+
+        int CountContained(string colour) {
+            if (Contains.ContainsKey(colour)) {
+                var count = 0;
+
+                var bags = Contains[colour];
+                foreach (var bag in bags.Keys) {
+                    count += bags[bag];
+                    count += bags[bag] * CountContained(bag);
+                }
+
+                return count;
+            }
+            return 0;
         }
     }
 }
