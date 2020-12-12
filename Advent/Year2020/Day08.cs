efb24d45a0db54447cd4c44adcfe9c5e924ca10a
Julian Melville <julian@melville.net.nz>
2020-12-12 22:37:38 +1300

    2020 day 8 part 2

--- Advent/Year2020/Day08.cs
+++ Advent/Year2020/Day08.cs
@@ -55,47 +55,13 @@ namespace Advent.Year2020 {
         }
 
         public override string PartTwo(string input) {
-            input = @"nop +0
-                    acc +1
-                    jmp +4
-                    acc +3
-                    jmp -3
-                    acc -99
-                    acc +1
-                    nop -4
-                    acc +6";
-
             var program = input.AsLines().ToList();
-            var accumulator = 0;
-            var iptr = 0;
-            var visited = new HashSet<int>();
 
-            while (true) {
-                visited.Add(iptr);
-                var instr = new Instruction(program[iptr]);
-
-                Out.Print($"iptr: {iptr}  acc: {accumulator}  instr: {instr}");
-
-                switch (instr.Operation) {
-                    case Opcode.ACC:
-                        accumulator += instr.Argument;
-                        iptr += 1;
-                        break;
-                    case Opcode.JMP:
-                        iptr += instr.Argument;
-                        break;
-                    case Opcode.NOP:
-                        iptr += 1;
-                        break;
-                }
-
-                if (iptr == program.Count) {
-                    Out.Print($"About to jump past the end of program, acc is {accumulator}");
-                    break;
-                }
-            }
-
-            return accumulator.ToString();
+            var vm = new VirtualMachine(program);
+            var result = vm.Run();
+            Out.Print(result);
+            
+            return  vm.Accumulator.ToString();
         }
     }
 
@@ -126,5 +92,66 @@ namespace Advent.Year2020 {
         }
 
         public override string ToString() => $"Instruction ({Operation}, {Argument})";
+    }
+
+    class VirtualMachine {
+        int _accumulator = 0;
+        int _iptr = 0;
+        int _targetLocation = 0;
+        HashSet<int> _visited = new();
+        List<string> _program;
+
+        public int Accumulator => _accumulator;
+
+        public VirtualMachine(List<string> program) {
+            _program = program;
+            _targetLocation = program.Count;
+        }
+
+        /// <summary>
+        /// Runs the program. Returns true if we jump to mem + 1, false if loop detected.
+        /// </summary>
+        public bool Run() {
+            Reset();
+            var hitTargetLocation = false;
+            var illegalMemoryLocation = false;
+
+            while (true) {
+                _visited.Add(_iptr);
+                var instr = new Instruction(_program[_iptr]);
+
+                Out.Print($"iptr: {_iptr}  acc: {_accumulator}  instr: {instr}");
+
+                switch (instr.Operation) {
+                    case Opcode.ACC:
+                        _accumulator += instr.Argument;
+                        _iptr += 1;
+                        break;
+                    case Opcode.JMP:
+                        _iptr += instr.Argument;
+                        break;
+                    case Opcode.NOP:
+                        _iptr += 1;
+                        break;
+                }
+
+                if (_iptr == _targetLocation) {
+                    Out.Print($"About to jump past the end of program, acc is {_accumulator}");
+                    hitTargetLocation = true;
+                    break;
+                } else if (_visited.Contains(_iptr)) {
+                    Out.Print($"Loop detected: about to repeat instruction {_iptr}");
+                    break;
+                }
+            }
+
+            return hitTargetLocation;
+        }
+
+        private void Reset() {
+            _accumulator = 0;
+            _iptr = 0;
+            _visited.Clear();
+        }
     }
 }
