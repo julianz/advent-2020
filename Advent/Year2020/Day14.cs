ab2a1b2d846960aa660d0f0827b42bcbf71af73a
Julian Melville <julian@melville.net.nz>
2020-12-20 15:06:10 +1300

    2020 day 14 part 2 solve

--- Advent/Year2020/Day14.cs
+++ Advent/Year2020/Day14.cs
@@ -46,12 +46,111 @@ namespace Advent.Year2020 {
         }
 
         public override string PartTwo(string input) {
-            input = @"mask = 000000000000000000000000000000X1001X
-                    mem[42] = 100
-                    mask = 00000000000000000000000000000000X0XX
-                    mem[26] = 1";
+            //input = @"mask = 000000000000000000000000000000X1001X
+            //        mem[42] = 100
+            //        mask = 00000000000000000000000000000000X0XX
+            //        mem[26] = 1";
 
-            throw new PuzzleNotSolvedException();
+            var program = input.AsLines();
+            var memory = new Dictionary<long, long>();
+            var mask = "";
+
+            foreach (var line in program) {
+                var pieces = line.SplitBySeparator("=").ToList();
+                if (pieces[0] == "mask") {
+                    // reset mask, split it into ones and zeroes masks
+                    mask = pieces[1];
+                    //Out.Print($"Setting mask to  {mask}");
+                } else {
+                    // set memory
+                    var originalAddress = Int64.Parse(pieces[0].Substring(4, pieces[0].Length - 5));
+                    var value = Int64.Parse(pieces[1]);
+
+                    // MAGIC HAPPENS
+
+                    // need to either transform the mask into every variant of itself as a binary
+                    // and figure out the operation that leaves 0's alone and overwrites 1's
+
+                    // OR
+
+                    // convert the original address into a binary string and do the transformation
+                    // in the string domain (slower but easier to debug)
+
+                    var source = GetBinaryString(originalAddress);
+                    //Out.Print($"Original address {source}");
+
+                    // apply the mask to the address 
+                    var masked = ApplyMaskToAddress(source, mask);
+                    //Out.Print($"Masked           {masked}");
+
+                    // produce the list of actual addresses that will have the value written to them.
+                    var addresses = GetAddressesFromMasked(masked);
+
+                    foreach (var addrString in addresses) {
+                        var address = Convert.ToInt64(addrString, 2);
+                        //Out.Print($"Setting {address} to {value}");
+                        memory[address] = value;
+                    }
+                }
+            }
+
+            return memory.Values.Sum().ToString();
+        }
+
+        private List<String> GetAddressesFromMasked(string masked) {
+            var addresses = new List<String>();
+            addresses.Add("");
+
+            foreach (var c in masked) {
+                if (c == '0' || c == '1') {
+                    // Append the 0/1 to every address in the list.
+                    for (var addr = 0; addr < addresses.Count; addr++) {
+                        addresses[addr] += c;
+                    }
+                } else {
+                    // An X means we have to double the list and.
+                    var copy = new List<String>(addresses);
+                    for (var addr = 0; addr < addresses.Count; addr++) {
+                        addresses[addr] += '0';
+                        copy[addr] += '1';
+                    }
+                    addresses.AddRange(copy);
+                }
+            }
+
+            return addresses;
+        }
+
+        string ApplyMaskToAddress(string source, string mask) {
+            if (source.Length != mask.Length) {
+                throw new ArgumentException("Lengths don't match");
+            }
+
+            var result = new StringBuilder(source.Length);
+
+            for (var c = 0; c < source.Length; c++) {
+                result.Append((mask[c] == '0') ? source[c] : mask[c]);
+            }
+
+            return result.ToString();
+        }
+
+        string GetBinaryString(long num, int length = 36) {
+            var position = length - 1;
+            var result = new StringBuilder(length);
+
+            while (position >= 0) {
+                var bit = Convert.ToInt64(Math.Pow(2, position));
+                if (num / bit == 0) {
+                    result.Append('0');
+                } else {
+                    result.Append('1');
+                    num -= bit;
+                }
+                position--;
+            }
+
+            return result.ToString();
         }
     }
 }
