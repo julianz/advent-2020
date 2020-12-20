8701ded421a42f736793f6b0c560d988cfc4f6da
Julian Melville <julian@melville.net.nz>
2020-12-20 18:13:38 +1300

    2020 day 16 input

--- Advent/Year2020/Day15.cs
+++ Advent/Year2020/Day15.cs
@@ -1,31 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using Advent.Util;
 
 namespace Advent.Year2020 {
     [Day(2020, 15)]
     public class Day15 : DayBase {
-        public Day15() {
-            NeedsInput = false;
-        }
-
-        string Input => "0,20,7,16,1,18,15";
-
         public override string PartOne(string input) {
             //input = "0,3,6"; // expecting 0 3 6 0 3 3 1 0 4 0, 2020th is 436
 
-            var numbers = Input.SplitBySeparator(",").Select(Int32.Parse).ToList();
+            var numbers = input.SplitBySeparator(",").Select(Int32.Parse).ToList();
             var lastSpoken = PlaySpeakingGame(numbers, turns: 2020);
 
             return lastSpoken.ToString();
         }
 
         public override string PartTwo(string input) {
-            var numbers = Input.SplitBySeparator(",").Select(Int32.Parse).ToList();
+            var numbers = input.SplitBySeparator(",").Select(Int32.Parse).ToList();
             var lastSpoken = PlaySpeakingGame(numbers, turns: 30000000);
 
             return lastSpoken.ToString();
---
+++ Advent/Year2020/Day16.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Advent.Util;
+
+namespace Advent.Year2020 {
+    [Day(2020, 16)]
+    public class Day16 : DayBase {
+        public override string PartOne(string input) {
+            throw new PuzzleNotSolvedException();
+        }
+
+        public override string PartTwo(string input) {
+            throw new PuzzleNotSolvedException();
+        }
+    }
+}
