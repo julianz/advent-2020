e0025d4dc53c28bdc0a89beabd5fc2fd6deb8b08
Julian Melville <julian@melville.net.nz>
2021-11-30 21:49:19 +1300

    Solve 2015 day 6b

--- Advent/Year2015/Day06.cs
+++ Advent/Year2015/Day06.cs
@@ -12,16 +12,18 @@ using Advent.Util;
 namespace Advent.Year2015 {
     [Day(2015, 6)]
     public class Day06 : DayBase {
+
+        private readonly Regex InputPattern = new Regex(@"(turn on|toggle|turn off) (\d+),(\d+) through (\d+),(\d+)", RegexOptions.Compiled);
+
+        private const int GridRows = 1000;
+        private const int GridCols = 1000;
+
         public override string PartOne(string input) {
 
-            var pattern = new Regex(@"(turn on|toggle|turn off) (\d+),(\d+) through (\d+),(\d+)");
-
-            var gridRows = 1000;
-            var gridCols = 1000;
-            var grid = new HashSet<int>(gridRows * gridCols);
+            var grid = new HashSet<int>(GridRows * GridCols);
 
             foreach (var line in input.AsLines()) {
-                var match = pattern.Match(line);
+                var match = InputPattern.Match(line);
                 if (!match.Success) {
                     throw new ArgumentException(line);
                 }
@@ -33,9 +35,10 @@ namespace Advent.Year2015 {
 
                 var command = match.Groups[1].Value;
 
+                int cell;
                 for (var y = fromY; y <= toY; y++) {
                     for (var x = fromX; x <= toX; x++) {
-                        var cell = (y * gridCols) + x;
+                        cell = (y * GridCols) + x;
 
                         switch (command) {
                             case "turn on":
@@ -62,7 +65,47 @@ namespace Advent.Year2015 {
         }
 
         public override string PartTwo(string input) {
-            throw new PuzzleNotSolvedException();
+            var grid = new Dictionary<int, int>(GridRows * GridCols);
+
+            foreach (var line in input.AsLines()) {
+                var match = InputPattern.Match(line);
+                if (!match.Success) {
+                    throw new ArgumentException(line);
+                }
+
+                var fromX = Convert.ToInt32(match.Groups[2].Value);
+                var fromY = Convert.ToInt32(match.Groups[3].Value);
+                var toX = Convert.ToInt32(match.Groups[4].Value);
+                var toY = Convert.ToInt32(match.Groups[5].Value);
+
+                var command = match.Groups[1].Value;
+
+                int cell;
+                for (var y = fromY; y <= toY; y++) {
+                    for (var x = fromX; x <= toX; x++) {
+                        cell = (y * GridCols) + x;
+
+                        switch (command) {
+                            case "turn on":
+                                // add 1
+                                grid[cell] = (grid.Keys.Contains(cell) ? grid[cell] : 0) + 1;
+                                break;
+
+                            case "turn off":
+                                // subtract 1 to a minimum of 0
+                                grid[cell] = Math.Max((grid.Keys.Contains(cell) ? grid[cell] : 0) - 1, 0);
+                                break;
+
+                            case "toggle":
+                                // add 2
+                                grid[cell] = (grid.Keys.Contains(cell) ? grid[cell] : 0) + 2;
+                                break;
+                        }
+                    }
+                }
+            }
+
+            return grid.Values.Sum().ToString();
         }
     }
 }
