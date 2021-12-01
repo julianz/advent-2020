21a65d5c78f61377939d59999182399509ce583c
Julian Melville <julian@melville.net.nz>
2021-12-01 19:14:17 +1300

    Solve 2021 day 1

--- Advent/Year2021/Day01.cs
+++ Advent/Year2021/Day01.cs
@@ -13,11 +13,36 @@ namespace Advent.Year2021 {
     [Day(2021, 1)]
     public class Day01 : DayBase {
         public override string PartOne(string input) {
-            throw new PuzzleNotSolvedException();
+
+            var depth = 99999; // so the first line isn't an increase...
+            var increaseCount = 0;
+
+            foreach (var reading in input.AsInts()) {
+                if (reading > depth) {
+                    increaseCount++;
+                }
+                depth = reading;
+            }
+
+            return increaseCount.ToString();
         }
 
         public override string PartTwo(string input) {
-            throw new PuzzleNotSolvedException();
+            var readings = input.AsInts().ToList();
+
+            var depth = 99999; // so the first line isn't an increase...
+            var increaseCount = 0;
+
+            for (var i = 0; i <= (readings.Count - 3); i++) {
+                var reading = readings[i] + readings[i + 1] + readings[i + 2];
+
+                if (reading > depth) {
+                    increaseCount++;
+                }
+                depth = reading;
+            }
+
+            return increaseCount.ToString();
         }
     }
 }
