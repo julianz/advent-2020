31f1bf6c77e3678d86f18b4cbb97d3598db89061
Julian Melville <julian@melville.net.nz>
2021-12-16 23:04:45 +1300

    Add simple memoization of function calls

---
+++ Advent/Util/MethodExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Advent.Util {
+    public static class MethodExtensions {
+        /// <summary>
+        /// Memoize a function call, as per https://www.aleksandar.io/post/memoization/
+        /// </summary>
+        public static Func<T, TResult> Memoize<T, TResult>(this Func<T, TResult> f) {
+            var cache = new ConcurrentDictionary<T, TResult>();
+            return a => cache.GetOrAdd(a, f);
+        }
+    }
+}
