b362b982b9e714a98fac91f619860ddfbb34eb65
Julian Melville <julian@melville.net.nz>
2021-12-21 15:22:46 +1300

    Add a string extension method to sort strings in char order

---
+++ Advent/Util/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Advent.Util {
+    /// <summary>
+    /// Extension methods for working with strings.
+    /// </summary>
+    public static class StringExtensions {
+        /// <summary>
+        /// Return the input string, sorted in character order
+        /// </summary>
+        public static string SortChars(this string input) {
+            char[] chars = input.ToCharArray();
+            Array.Sort(chars);
+            return new string(chars);
+        }
+    }
+}
