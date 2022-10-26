9b40fd9cda6c18f55a1de1288ba67caa4da236de
Julian Melville <julian.melville@aderant.com>
2022-10-27 11:05:40 +1300

    Comments in config class
    Because I never remember how this works when I come back to it after a while

--- Advent/Config.cs
+++ Advent/Config.cs
@@ -1,14 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Advent {
     public class Config {
+        /// <summary>
+        /// The default year to use if one is not provided on the command line.
+        /// </summary>
         public int DefaultYear { get; set; } = 2021;
+
+        /// <summary>
+        /// The session cookie from adventofcode.com.
+        /// </summary>
         public string SessionCookie { get; set; } = "";
+
+        /// <summary>
+        /// Root directory that other paths are calculated from.
+        /// </summary>
         public string ApplicationDirectory { get; set; } = "";
+
+        /// <summary>
+        /// Where to look for puzzle input, either as an absolute path or relative to ApplicationDirectory.
+        /// </summary>
         public string InputDirectory { get; set; } = "Input";
+
+        public void SanityCheck() {
+            if (DefaultYear < 2015) {
+                throw new ArgumentOutOfRangeException(nameof(DefaultYear), "Config: Default year must be defined and >= 2015");
+            }
+
+            if (SessionCookie == null) {
+                throw new ArgumentOutOfRangeException(nameof(SessionCookie), "Config: SessionCookie must be defined");
+            }
+
+            if (SessionCookie == null) {
+                throw new ArgumentOutOfRangeException(nameof(ApplicationDirectory), "Config: ApplicationDirectory must be defined");
+            }
+
+            if (SessionCookie == null) {
+                throw new ArgumentOutOfRangeException(nameof(InputDirectory), "Config: InputDirectory must be defined");
+            }
+        }
     }
 }
