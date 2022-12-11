8ddde029d01728b01edaa82dd07c18b7e8795fc9
Julian Melville <julian.melville@aderant.com>
2022-12-11 15:19:33 +1300

    Add some operator overrides to Coords

--- Util/Types.cs
+++ Util/Types.cs
@@ -2,5 +2,18 @@
     /// <summary>
     /// Helper type to represent integer X and Y coordinates
     /// </summary>
-    public record struct Coords(int X, int Y);
+    public record struct Coords(int X, int Y) {
+
+        public static Coords operator +(Coords c) 
+            => c;
+
+        public static Coords operator -(Coords c) 
+            => new Coords(-c.X, -c.Y);
+
+        public static Coords operator +(Coords a, Coords b) 
+            => new Coords(a.X + b.X, a.Y + b.Y);
+
+        public static Coords operator -(Coords a, Coords b)
+            => a + -b;
+    }
 }
